c69674aa22e11324eec3e14b9f2f4739cb5c43f5
306bobby-android <306bobby-android@users.noreply.github.com>
2026-08-11 02:20:47 -0400

    SetupWizardActivity: Remove finish() and setResult() to align with standard SetupWizard forward navigation

--- app/src/main/java/org/microg/installer/updater/SetupWizardActivity.kt
+++ app/src/main/java/org/microg/installer/updater/SetupWizardActivity.kt
@@ -99,13 +99,10 @@ class SetupWizardActivity : AppCompatActivity() {
             if (intent.hasExtra("theme")) {
                 nextIntent.putExtra("theme", intent.getStringExtra("theme"))
             }
-            nextIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(nextIntent)
         } catch (e: Exception) {
             Log.e("SetupWizardActivity", "Could not start next wizard action", e)
-            setResult(resultCode)
         }
-        finish()
     }
 
     private fun setAppLauncherEnabled(enabled: Boolean) {
